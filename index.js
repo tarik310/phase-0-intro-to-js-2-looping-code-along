006ec9b5be8426fa118eb30384c86bfe14e145e4
Tarik <88586285+tarik310@users.noreply.github.com>
2023-03-20 13:35:19 +0300

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, occasion) {
+    const messages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${occasion} gift!`);
+    }
+  
+    return messages;
+}
+
+function countDown(number){
+    for (let i = number; i>=0;i--){
+        console.log(i)
+    }
+}
